717a82809770af19b52d6f6e1a92c89ffc258ca2
Andrei Costin <dDevil@Andreis-MacBook.lan>
2016-09-17 13:38:27 +0100

    First factory in place

--- js/controllers/inbox.js
+++ js/controllers/inbox.js
@@ -1,6 +1,18 @@
 angular.module('EmailApp')
-    .controller('InboxCtrl',
-     function InboxCtrl() {
-         'use strict';
-         console.log('Inbox loated!')
-     });
+  .controller('InboxCtrl',
+    function InboxCtrl ( $scope, InboxFactory ) {
+      'use strict';
+      $scope.cucu = "cucu!";
+      $scope.meta = {
+        title: "My Inbox"
+      };
+      InboxFactory.getMessages()
+        .success(function(jsonData, statusCode){
+            console.log('The request was successful!', statusCode);
+            console.dir(jsonData);
+            // Now add the Email messages to the controller's scope
+            $scope.data = {
+              emails: jsonData
+            };
+        });
+    });
